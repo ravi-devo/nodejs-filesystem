25307be6c1798f2e67f63b087ae75c561388ce5e
ravi-devo <ravi.ravichandran370@gmail.com>
2023-12-12 11:41:13 +0530

    date time, readme file updated

---
+++ README.md
@@ -0,0 +1,5 @@
+The objective of this task is to create file while calling the API and read the file from a particular folder from calling the API.
+
+To call the createFile API first the File_System folder should be available
+
+The two endpoints, Github URL and Render URL are available on the URL-File.txt
--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const fs = require("fs");
 
 const app = express();
 
-app.post("/createFile", (req, res) => {
+let currentdate = new Date();
+let datetime = currentdate.getDate() + "-" + (currentdate.getMonth()+1)
+    + "-" + currentdate.getFullYear() + "_"
+    + currentdate.getHours() + "h"
+    + currentdate.getMinutes() + "min";
+// console.log("Date Time: ", datetime);
 
-    fs.writeFile("./File_System/date-time.txt", `${new Date().toLocaleString()}`, (error, data) => {
+app.post("/createFile", (req, res) => {
+    fs.writeFile(`./File_System/${datetime}.txt`, `${new Date().toLocaleString()}`, (error, data) => {
         if (error) {
             return res.status(500).send({ message: "Error creating a file: ", error });
         }
@@ -17,8 +23,8 @@ app.post("/createFile", (req, res) => {
 app.get("/getFile", (req, res) => {
 
     fs.readdir("./File_System", (error, data) => {
-        if(error){
-            return res.status(400).send({message: "Error fetching the file", error});
+        if (error) {
+            return res.status(400).send({ message: "Error fetching the file", error });
         }
         res.status(200).send({ message: "File fetched successfully", data });
         console.log("Data: ", data);
